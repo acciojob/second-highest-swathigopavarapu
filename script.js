7f4101949d1fcd5fc8b1b83d4104df102d9f2d8f
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-25 11:59:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) 
+	  return -Infinity;
+
+  let unique = [...new Set(arr)];
+
+  if (unique.length < 2)
+	  return -Infinity; 
+
+  unique.sort((a, b) => b - a);
+
+  return unique[1]; 
+}
