77ae1e87841e9eeaf683b0cf80dff84cb5abf3c7
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-30 11:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function secondHighest(arr) {
   if (!arr || arr.length < 2) return -Infinity;
 
+  // Remove duplicates
+  let uniqueArr = [...new Set(arr)];
+
+  if (uniqueArr.length < 2) return -Infinity;
+
   let max = -Infinity;
   let second = -Infinity;
 
-  for (let num of arr) {
+  for (let num of uniqueArr) {
     if (num > max) {
       second = max;
       max = num;
-    } else if (num < max && num > second) {
+    } else if (num > second && num < max) {
       second = num;
     }
   }
 
-  return second === -Infinity ? -Infinity : second;
+  return second;
 }
